93f99daf569b3dae56bba2f91325b66dfdb90789
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-06-08 01:00:04 +0300

    class-11

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const users = require('../models/user-model');
+const base64 = require('base-64');
+
+module.exports = (req, res, next) => {
+    if (!req.headers.authorization) {
+        next('invalid Login');
+        return;
+    }
+    console.log("req.headers.authorization >>>> ",req.headers.authorization)
+    let basic = req.headers.authorization.split(' ').pop();
+
+    let [user, pass] = base64.decode(basic).split(':');
+    users.authenticateBasic(user, pass).then(validUser => {
+        req.token = users.generateToken(validUser);
+        next();
+    })
+    .catch(err => next('Invalid Login!!'));
+
+}
