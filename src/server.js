a6143d3b843921973527314efa7658d9ad99afda
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-06-10 17:11:35 +0300

    asd

---
+++ src/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(express.static('./public'));
+// app.use('/', express.static('../public'));
+app.use(express.json()); // body
+app.use(morgan('dev'));
+// const routeapi=require('./auth/router');
+// app.use(routeapi);
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => { console.log(`Listening on port ${port}`); });
+  },
+};
